bcb7637c5b08fc7c093979c1920cc7fa8890435d
asisgtm <mayakomantra@gmail.com>
2020-07-03 05:20:20 +0000

    Local library intitiation

--- localllibrary/localllibrary/settings.py
+++ localllibrary/localllibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't1l1-!-cfcvm4hnr))rp7u2!u7t0j&usqe^bsnm69d9=n7ik6p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
